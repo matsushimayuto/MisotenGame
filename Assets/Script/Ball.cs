43eba00edf6d04c206fb49559594b20320182cac
matsushimayuto <mheyho914@gmail.com>
2025-09-17 14:56:56 +0900

    弾の挙動を修正

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -10,16 +10,23 @@ public class Ball : MonoBehaviour
     public string m_sTargetTag = string.Empty;  // 9/16:敵のタグを一時的に"Respawn"にしているので競合解決後"Enemy"を追加し直すこと
 
     private float m_fTimeCount = 0.0f;
+    private Rigidbody rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); // 取得
     }
 
     // Update is called once per frame
     void Update()
     {
+        // フィールドの傾き方向に動かす
+        if (Input.GetKeyDown(KeyCode.W)) rb.AddForce(new Vector3(0.0f, 0.0f, 200.0f));    // 前
+        if (Input.GetKeyDown(KeyCode.S)) rb.AddForce(new Vector3(0.0f, 0.0f, -200.0f));    // 後
+        if (Input.GetKeyDown(KeyCode.A)) rb.AddForce(new Vector3(-200.0f, 0.0f, 0.0f));    // 左
+        if (Input.GetKeyDown(KeyCode.D)) rb.AddForce(new Vector3(200.0f, 0.0f, 0.0f));    // 右
+
         // 指定時間経ったらオブジェクトを消す
         m_fTimeCount += Time.deltaTime;
         if(m_fTimeCount > m_fDestroyTime)
