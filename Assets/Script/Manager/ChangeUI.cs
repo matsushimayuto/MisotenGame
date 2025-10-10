bf5d63161d8b9c434176a07e6d18a2f2e239769d
BannoSouya <banyechuangmi@gmail.com>
2025-10-10 12:57:37 +0900

    セーブロードができるようにしました

--- Assets/Script/Manager/ChangeUI.cs
+++ Assets/Script/Manager/ChangeUI.cs
@@ -3,6 +3,8 @@ using UnityEngine.XR;
 
 public class ChangeUI : MonoBehaviour
 {
+    public SaveLoad saveload;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,14 +14,18 @@ public class ChangeUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-            GameManager.Instance.ChangeState(GameState.Playing);
-        if (Input.GetKeyDown(KeyCode.W))
-            GameManager.Instance.ChangeState(GameState.Paused);
-        if (Input.GetKeyDown(KeyCode.E))
-            GameManager.Instance.ChangeState(GameState.Result);
-        if (Input.GetKeyDown(KeyCode.R))
-            GameManager.Instance.ChangeState(GameState.GameOver);
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //    GameManager.Instance.ChangeState(GameState.Playing);
+        //if (Input.GetKeyDown(KeyCode.W))
+        //    GameManager.Instance.ChangeState(GameState.Paused);
+        //if (Input.GetKeyDown(KeyCode.E))
+        //    GameManager.Instance.ChangeState(GameState.Result);
+        //if (Input.GetKeyDown(KeyCode.R))
+        //    GameManager.Instance.ChangeState(GameState.GameOver);
 
+        if(Input.GetKeyDown(KeyCode.S))
+            saveload.Save();
+        if (Input.GetKeyDown(KeyCode.L))
+            saveload.DataLoad();
     }
 }
--- Assets/Script/Manager/SaveLoad.cs
+++ Assets/Script/Manager/SaveLoad.cs
@@ -1,16 +1,47 @@
+using System.IO;
 using UnityEngine;
 
 public class SaveLoad : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [System.Serializable]
+    class StageData
     {
-        
+        public float playerPoint;  // 最後にクリアしたステージの場所のデータ
+        public bool[] ClearFlag = new bool[4];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Save()
     {
-        
+        StageData stageData = new StageData();
+        // 保存したいデータの値を入れる
+        stageData.playerPoint = 100f;
+        for(int i = 0; i < 4; i++)
+            stageData.ClearFlag[i] = true;
+
+        string json = JsonUtility.ToJson(stageData, true);
+        File.WriteAllText(Application.persistentDataPath + "/save.json", json);
+        Debug.Log(Application.persistentDataPath);
     }
+
+    public void DataLoad()
+    {
+        string path = Application.persistentDataPath + "/save.json";
+        if(File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            StageData stageData = JsonUtility.FromJson<StageData>(json);
+
+            Debug.Log(stageData.playerPoint);
+
+            for (int i = 0; i < 4; i++)
+                Debug.Log("ステージ" + i + stageData.ClearFlag[i]);
+        }
+        else
+        {
+            Debug.Log("セーブデータがありません");
+        }
+
+    }
+
+
 }
