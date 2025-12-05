51a719c248f995a83929e546b93c24ada7c0f7db
BannoSouya <banyechuangmi@gmail.com>
2025-12-05 09:51:19 +0900

    csvファイル読み込みをStageごとに使用しやすいように変更

--- Assets/Script/Select/Select.cs
+++ Assets/Script/Select/Select.cs
@@ -4,12 +4,34 @@ public class Select : MonoBehaviour
 {
     void Start()
     {
-        
+        //// フォルダ内のステージcsvを読み込んで、配列に格納しておく関数
+        //void LoadAllStages()
+        //{
+        //    // Stageフォルダ内のcsvを全取得
+        //    TextAsset[] stageFiles = Resources.LoadAll<TextAsset>(StageFolderPath);
+
+        //    foreach (var csv in stageFiles)
+        //    {
+        //        Debug.Log($"読み込み: {csv.name}");
+
+        //        string csvText = csv.text;
+        //        ParseCSV(csv.name, csvText);
+        //    }
+        //}
+
+        //void ParseCSV(string csvName, string csvText)
+        //{
+
+        //}
+
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-            SceneLoader.Instance.LoadScene(SceneName.Stage, true, 2f);
+        //switch()
+        //{
+        //    case 0:
+        //        break;
+        //}
     }
 }
