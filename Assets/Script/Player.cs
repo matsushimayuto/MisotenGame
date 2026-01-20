c7211eeb2e51d3962889c47e97183958557778fb
masaki1298 <masakiki31@gmail.com>
2026-01-20 14:46:36 +0900

    進行不可のバグ修正、SkyBoxがずっと回転しているように修正

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Player : MonoBehaviour
 {
@@ -55,9 +56,34 @@ public class Player : MonoBehaviour
         {
             if (collision.rigidbody.isKinematic == false)//ブロックが動いているとき
             {
-                Destroy(gameObject);    //破壊
+                //Destroy(gameObject);    //破壊
+                Block block = collision.gameObject.GetComponent<Block>();
+                if (block == null) return;
+                // 「ブロックが動いている場合のみ」
+                if (block.CheckMove())
+                {
+                    block.ForceLockStop();
+                }
+                Time.timeScale = 0.0f;
+                GameManager.Instance.ChangeState(GameState.GameOver);
+                // Start表示中の遅延処理
+                StartCoroutine(Delay(3.0f, () =>
+                {
+                    // ゲームを再開
+                    Time.timeScale = 1f;
+                    // StartUIを非表示にする
+                    GameManager.Instance.IsFirstStageEnter = false;
+                    // シーン遷移
+                    SceneLoader.Instance.LoadScene(SceneName.Stage, true, 2.0f);
+                }));
             }
         }
+    }
+
+    private IEnumerator Delay(float time, System.Action action)
+    {
+        yield return new WaitForSecondsRealtime(time);
+        action?.Invoke();
     }
 
 }
